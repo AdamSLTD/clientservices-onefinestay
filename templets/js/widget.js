3d829ce9844e1b7c90fc9fb18b5d32c4d9f0202c
Adam <lehreradam@yahoo.com>
2020-01-14 15:47:30 -0800

    testing second widget file link

--- templets/js/widget.js
+++ templets/js/widget.js
@@ -25,7 +25,7 @@ var IMIChatInit = function () {
     const _skey = "70003df6-33d8-11ea-afcd-0610d74d64fc";
     return {
         domainName: function () {
-            return "/widget.js";
+            return "/widge1t.js";
         },
         init: function () {
             try {
